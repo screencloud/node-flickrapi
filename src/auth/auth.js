063320f654e85055120d530bd6c02af66e3802a6
luke <luke@codegent.com>
2016-11-01 11:35:14 +0700

    Update auth.js

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,9 +1,7 @@
 module.exports = (function() {
   "use strict";
 
-  var open = require("open"),
-      prompt = require("prompt"),
-      ExchangeTokens = require("./exchange");
+  // ExchangeTokens = require("./exchange");
 
   /**
    * Second part of oauth: request authorization
@@ -14,24 +12,7 @@ module.exports = (function() {
         oauth_token_secret = options.oauth_token_secret,
         authURL = "https://www.flickr.com/services/oauth/authorize",
         browserURL = authURL + "?oauth_token=" + oauth_token + "&perms=" + options.permissions;
-
-    // are we in the browser?
-    if(!options.nobrowser) { open(browserURL); }
-    else { console.log("please visit " + browserURL); }
-
-    if(options.callback === "oob") {
-      prompt.start();
-      prompt.get(['oauth_verifier'], function(err, res) {
-        options.oauth_verifier = res.oauth_verifier.trim();
-        new ExchangeTokens(options, requestCompleted);
-      });
-    }
-    else {
-      options.exchange = function(tokens) {
-        options.oauth_verifier = tokens.oauth_verifier;
-        new ExchangeTokens(options, requestCompleted);
-      };
-    }
+    return browserURL; 
   };
 
   return RequestAuthorization;
