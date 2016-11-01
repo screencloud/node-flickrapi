e434205156ab620d0c26bf720181b789cba3cf41
Jirasak Saebang <cosmodesktop@gmail.com>
2016-11-01 14:22:46 +0700

    Update FlickrAPI.js

--- src/FlickrAPI.js
+++ src/FlickrAPI.js
@@ -1,103 +1,99 @@
+"use strict";
 
+var request = require("request");
+var Utils = require("./utils");
 
-module.exports = (function Flickr() {
-  "use strict";
+/**
+ * The authentication function will sign into flickr as
+ * an app, obtaining authorization keys values if it
+ * does not already have them.
+ */
+var getAuthRedirectURL = function(options, callback) {
 
-  var request = require("request");
-  var Utils = require("./utils");
+  options = Object.assign({}, options)
 
-  /**
-   * The authentication function will sign into flickr as
-   * an app, obtaining authorization keys values if it
-   * does not already have them.
-   */
-  var getAuthRedirectURL = function(options, callback) {
+  if(!options.requestOptions) options.requestOptions = {};
 
-    options = Object.assign({}, options)
+  options = Utils.setAuthVals(options);
 
-    if(!options.requestOptions) options.requestOptions = {};
+  var queryArguments = {
+        oauth_callback:         options.callback,
+        oauth_consumer_key:     options.api_key,
+        oauth_nonce:            options.oauth_nonce,
+        oauth_timestamp:        options.oauth_timestamp,
+        oauth_signature_method: "HMAC-SHA1",
+        oauth_version:          "1.0"
+      };
 
-    options = Utils.setAuthVals(options);
+  var queryString = Utils.formQueryString(queryArguments);
+  var data = Utils.formBaseString("GET", this.url, queryString);
+  var signature = Utils.sign(data, options.secret);
 
-    var queryArguments = {
-          oauth_callback:         options.callback,
-          oauth_consumer_key:     options.api_key,
-          oauth_nonce:            options.oauth_nonce,
-          oauth_timestamp:        options.oauth_timestamp,
-          oauth_signature_method: "HMAC-SHA1",
-          oauth_version:          "1.0"
-        };
+  var flickrURL = "https://www.flickr.com/services/oauth/request_token" + "?" + queryString + "&oauth_signature=" + signature;
+  request.get(flickrURL, function(error, response, body) {
 
-    var queryString = Utils.formQueryString(queryArguments);
-    var data = Utils.formBaseString("GET", this.url, queryString);
-    var signature = Utils.sign(data, options.secret);
+    if(error) return callback(error);
 
-    var flickrURL = "https://www.flickr.com/services/oauth/request_token" + "?" + queryString + "&oauth_signature=" + signature;
-    request.get(flickrURL, function(error, response, body) {
-
-      if(error) return callback(error);
-
-      // show response
-      response = Utils.parseRestResponse(body);
-      if(!response) {
-        return callback(new Error("bad response"));
-      }
-      if(response.oauth_problem) {
-        // Occasionally, this will fail.
-        // Rerunning it then succeeds just fine.
-        return;
-      }
-      Object.keys(response).forEach(function(key) {
-        options[key] = response[key];
-      });
-      options.permissions = options.permissions || "read";
-      var oauth_token = options.oauth_token,
-        oauth_token_secret = options.oauth_token_secret,
-        authURL = "https://www.flickr.com/services/oauth/authorize",
-        browserURL = authURL + "?oauth_token=" + oauth_token + "&perms=" + options.permissions;
-      return callback(false,browserURL);
+    // show response
+    response = Utils.parseRestResponse(body);
+    if(!response) {
+      return callback(new Error("bad response"));
+    }
+    if(response.oauth_problem) {
+      // Occasionally, this will fail.
+      // Rerunning it then succeeds just fine.
+      return;
+    }
+    Object.keys(response).forEach(function(key) {
+      options[key] = response[key];
     });
+    options.permissions = options.permissions || "read";
+    var oauth_token = options.oauth_token,
+      oauth_token_secret = options.oauth_token_secret,
+      authURL = "https://www.flickr.com/services/oauth/authorize",
+      browserURL = authURL + "?oauth_token=" + oauth_token + "&perms=" + options.permissions;
+    return callback(false,browserURL);
+  });
+};
+
+var processAuthCallback = function(options, tokens, callback){
+
+  options = Object.assign({}, options)
+  options = Utils.setAuthVals(options);
+
+  var queryArguments = {
+    oauth_consumer_key:     options.api_key,
+    oauth_nonce:            options.oauth_nonce,
+    oauth_signature_method: "HMAC-SHA1",
+    oauth_timestamp:        options.oauth_timestamp,
+    // new values:
+    oauth_token: options.oauth_token,
+    oauth_verifier: tokens.oauth_verifier
   };
 
-  var processAuthCallback = function(options, tokens, callback){
+  var queryString = Utils.formQueryString(queryArguments);
+  var data = Utils.formBaseString("GET", this.url, queryString);
+  var signature = Utils.sign(data, options.secret, options.oauth_token_secret);
+  var flickrURL = "https://www.flickr.com/services/oauth/access_token" + "?" + queryString + "&oauth_signature=" + signature;
+  request.get(flickrURL, function(error, response, body) {
+    if(error) return callback(error);
+    return callback(false, Utils.parseRestResponse(body));
+  });
+};
 
-    options = Object.assign({}, options)
-    options = Utils.setAuthVals(options);
+var getAPI = function(options, callback) {
+  options = Object.assign({}, options)
+  if(!options.requestOptions) options.requestOptions = {};
+  var APIBuilder = require("./flickr-api-object");
+  new APIBuilder(options, Utils, callback);
+};
 
-    var queryArguments = {
-      oauth_consumer_key:     options.api_key,
-      oauth_nonce:            options.oauth_nonce,
-      oauth_signature_method: "HMAC-SHA1",
-      oauth_timestamp:        options.oauth_timestamp,
-      // new values:
-      oauth_token: options.oauth_token,
-      oauth_verifier: tokens.oauth_verifier
-    };
+/**
+ * The initial Flickr access point.
+ */
+module.exports = {
+  getAuthRedirectURL: getAuthRedirectURL,
+  processAuthCallback: processAuthCallback,
+  getAPI: getAPI
+};
 
-    var queryString = Utils.formQueryString(queryArguments);
-    var data = Utils.formBaseString("GET", this.url, queryString);
-    var signature = Utils.sign(data, options.secret, options.oauth_token_secret);
-    var flickrURL = "https://www.flickr.com/services/oauth/access_token" + "?" + queryString + "&oauth_signature=" + signature;
-    request.get(flickrURL, function(error, response, body) {
-      if(error) return callback(error);
-      return callback(false, Utils.parseRestResponse(body));
-    });
-  };
-
-  var getAPI = function(options, callback) {
-    options = Object.assign({}, options)
-    if(!options.requestOptions) options.requestOptions = {};
-    var APIBuilder = require("./flickr-api-object");
-    new APIBuilder(options, Utils, callback);
-  };
-
-  /**
-   * The initial Flickr access point.
-   */
-  return {
-    getAuthRedirectURL: getAuthRedirectURL,
-    processAuthCallback: processAuthCallback,
-    getAPI: getAPI
-  };
-
-}());
